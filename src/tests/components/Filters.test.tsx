9018ad131e613e9aada3fb743cda974c8f74eed0
Jeanie Lam <jlam@metaphaseconsulting.com>
2023-02-06 15:50:29 -0500

    Roughly added coverage and fixed test setup

--- src/tests/components/Filters.test.tsx
+++ src/tests/components/Filters.test.tsx
@@ -1,10 +1,17 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
 import Filters from '../../components/ForecastList/Filters/Filters';
 
 
 describe('Filters Component', () => {
     it('renders filter on page', () => {
-        render(<Filters />);
-        expect(screen.getByTestId("filters-column")).toBeInTheDocument();
+        let filters = {};
+        const updateFilters = (val: any) => { filters = val }
+        render(
+            <Filters
+                updateFilters={(_event, _field, value) => { updateFilters(value) }}
+                getFilterIndex={(_field, _value) => { return }}
+            />
+        );
+        expect(screen.getByTestId('filters-column')).toBeInTheDocument();
     });
 })
