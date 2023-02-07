dfdb67ec681574116e1c0d790f6dc5d118112f12
Jeanie Lam <jlam@metaphaseconsulting.com>
2023-02-07 13:03:02 -0500

    Added data conversions

--- src/components/ForecastList/Card/ListingCard/ListingCard.tsx
+++ src/components/ForecastList/Card/ListingCard/ListingCard.tsx
@@ -16,13 +16,34 @@ const ListingCard: React.FC<Props> = (props) => {
 
     const updated = data.updated ? ((data.updated.getMonth() + 1) + '/' + data.updated.getDate() + '/' + data.updated.getFullYear()) : '';
 
+    const convertNumber = (number: string) => {
+        if (number.slice(0, 7) == 'DOS-OPP') {
+            return number.slice(7, number.length);
+        }
+        return number;
+    }
+
+    const convertDuration = (duration?: string | null) => {
+        if (duration) {
+            if (duration == 'One year or less') return '<1 y';
+            if (duration == 'One to three years') return '1-3 y';
+            if (duration == 'Three to five years') return '3-5 y';
+            if (duration == 'More than five years') return '>5 y';
+            if (duration == '1 time purchase') return '1 time';
+            if (duration.substring(duration.length - 5) == 'years') {
+                return duration.substring(0, duration.indexOf(' ')) + ' y';
+            }
+        }
+        return undefined;
+    }
+
     return (
         <div className={classes.ListingCard}>
             <div className={classes.ListingCardInner}>
                 <div className={classes.ListingCardHeader}>
-                    <GlanceId value={data.number} />
+                    <GlanceId value={convertNumber(data.number)} />
                     <div className={classes.Title}>
-                        <a href={'/forecast/' + data.number}>
+                        <a href={'/forecast/' + convertNumber(data.number)}>
                             {data.requirement_description}
                         </a>
                     </div>
@@ -49,7 +70,7 @@ const ListingCard: React.FC<Props> = (props) => {
                     <div className={classes.ListingCardRight}>
                         <GlanceField inline label="Fiscal Year" data={data.fiscal_year ?? ''} />
                         <GlanceField inline label="Target Award Quarter" data={data.target_award_quarter ?? ''} labelWide />
-                        <GlanceField inline label="Length of Performance" data={data.length_of_performance ?? ''} labelWide dataWide />
+                        <GlanceField inline label="Length of Performance" data={convertDuration(data.length_of_performance) ?? ''} labelWide dataWide />
                         {/*<GlanceField inline label="Security Clearance" data={data.security_clearance} labelWide dataWide />*/}
                     </div>
                 </div>
